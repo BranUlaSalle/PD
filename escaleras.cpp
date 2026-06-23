54e2fbe623bb135aa05f0af14cbc7564689f3274
Bran <bpazp@ulasalle.edu.pe>
2026-06-23 00:33:38 -0500

    agrego comentarios y muestro la tabla dp

--- escaleras.cpp
+++ escaleras.cpp
@@ -9,6 +9,8 @@ void escalera(int n) {
 
     //crea un vector dp de tamaño n+1 para almacenar el número de formas de llegar a cada escalón
     vector<long long> dp(n + 1, 0);
+
+    //dp[0] = 1 para que 1 este en el inicio y dp[1] = 1 para que 1 este en el primer escalon
     dp[0] = 1;
     if (n >= 1) {
         dp[1] = 1;
@@ -16,9 +18,13 @@ void escalera(int n) {
     for (int i = 2; i <= n; ++i) {
         dp[i] = dp[i - 1] + dp[i - 2];
     }
-    
     cout << "Formas posibles: " << dp[n] << endl;
 
+    cout << "Tabla dp: ";
+    for (int i = 0; i <= n; ++i) {
+        cout << dp[i];
+        if (i < n) cout << " ";
+    }
 }
 
 int main() {
