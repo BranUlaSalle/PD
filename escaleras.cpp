f06bccdc6345acd14f857ffc044ac023a12084c5
Bran <bpazp@ulasalle.edu.pe>
2026-06-23 00:26:35 -0500

    probando funcion

--- escaleras.cpp
+++ escaleras.cpp
@@ -2,7 +2,17 @@
 #include <vector>
 using namespace std;
 
+//Funcion para calcular las formas de subir N escalones
 void escalera(int n) {
+    //crea un vector dp de tamaño n+1 para almacenar el número de formas de llegar a cada escalón
+    vector<long long> dp(n + 1, 0);
+    dp[0] = 1;
+    if (n >= 1) {
+        dp[1] = 1;
+    }
+    for (int i = 2; i <= n; ++i) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
 }
 
 int main() {
