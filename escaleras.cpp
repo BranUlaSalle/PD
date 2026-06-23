2ed942bb1ef73eaa40ee9536e2b8155aa6257ec9
Bran <bpazp@ulasalle.edu.pe>
2026-06-23 00:23:18 -0500

    agrego escalera y cambio ademas pongo funcion principal en escalera

---
+++ escaleras.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void escalera(int n) {
+}
+
+int main() {
+    int n;
+    cout << "Ingrese el numero de escalones N: ";
+    cin >> n;
+    escalera(n);
+    return 0;
+}
